e1f84f91cb147386e6b2a5e3405a7ff11d69af68
Sébastien Grignard <seb@amakaze.org>
2018-11-15 20:57:00 +0100

    chore: define __all__ for Sphinx

--- serpyco/__init__.py
+++ serpyco/__init__.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 
+__all__ = (
+    "Serializer",
+    "FieldEncoder",
+    "BaseSerpycoError",
+    "ValidationError",
+    "JsonSchemaError",
+    "NoEncoderError",
+    "post_dump",
+    "post_load",
+    "pre_dump",
+    "pre_load",
+    "Validator",
+    "field",
+    "string_field",
+    "number_field",
+    "StringFormat",
+)
+
 from serpyco.decorator import post_dump, post_load, pre_dump, pre_load
 from serpyco.exception import (
     BaseSerpycoError,
