5f04e5be579dcbd1c2e10d7f827fca5ab35f0a17
Sébastien Grignard <seb@amakaze.org>
2020-02-25 21:44:04 +0100

    chore: update package classifiers

--- setup.py
+++ setup.py
@@ -33,12 +33,13 @@ setup(
     install_requires=requires,
     license="MIT",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries",
     ],
     ext_modules=[
